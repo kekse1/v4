e8567e0c8319963d3aedec2e69225779008098e4
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2025-07-07 14:22:26 +0200

    1751890943757650203

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,7 +3,7 @@
 /*
  * Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
  * https://kekse.biz/ https://github.com/kekse1/v4/
- * v0.5.0
+ * v0.5.1
  *
  * Helper script for my v4 project @ https://github.com/kekse1/v4/.
  *
@@ -105,8 +105,6 @@ const finish = (_stream) => {
 const chunk = (_chunk) => {
 	if(_chunk === null)
 	{
-		stream = null;
-
 		if(state.sub)
 		{
 			line();
@@ -116,7 +114,8 @@ const chunk = (_chunk) => {
 		{
 			pushItem();
 		}
-		
+
+		stream = null;
 		return finish();
 	}
 	

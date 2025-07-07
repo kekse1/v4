ff5cdfcf5aec82ddf74cacd300210ade30d04688
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2025-07-07 09:02:49 +0200

    1751871767212441646

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,7 +3,7 @@
 /*
  * Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
  * https://kekse.biz/ https://github.com/kekse1/v4/
- * v0.1.0
+ * v0.2.0
  *
  * Helper script for my v4 project @ https://github.com/kekse1/v4/.
  *
@@ -11,7 +11,9 @@
  * 	(a) the 'blog.json' INDEX (for ~blog dynamics)
  * 	(b) every entry's 'body', each as '*.txt'
  *
- * TODO!
+ *
+ * TODO!!1 ...
+ *
  */
 
 //
@@ -26,7 +28,8 @@ import { ready } from '../js/lib.js';
 // --json
 // --body (dir 4 .txt)
 //
-var args = null; ready(() => main(args = getopt()));
+var args = null, items = [];
+ready(() => main(args = getopt()));
 
 const main = (_args = args) => {
 	//
@@ -40,26 +43,167 @@ const main = (_args = args) => {
 			autoClose: true,
 			emitClose: true,
 			highWaterMark: DEFAULT_BUFFER });
-	stream.once('end', (... _a) => finish(
-		stream, ... _a));
+	stream.once('end', (... _a) => chunk(
+		stream, null, ... _a));
 	stream.on('data', (... _a) => chunk(
 		stream, ... _a));
 };
 
 const finish = (_stream) => {
-	console.dir('fin');
+	console.dir({ items, length: items.length });
 };
 
 const chunk = (_stream, _chunk) => {
-	console.dir(_chunk.length);
+	if(_chunk === null)
+	{
+		if(state.body)
+		{
+			pushItem();
+		}
+		
+		return finish();
+	}
+	
 	parse(_chunk);
 };
 
 const parse = (_chunk) => {
-	//
+	loop: for(var i = 0; i < _chunk.length; ++i)
+	{
+		if(state.comment)
+		{
+			if(_chunk[i] === '\n')
+			{
+				if(_chunk[i + 1] === '\r')
+				{
+					++i;
+				}
+
+				state.comment = false;
+			}
+			
+			if(_chunk[i] === '\r')
+			{
+				if(_chunk[i + 1] === '\n')
+				{
+					++i;
+				}
+
+				state.comment = false;
+			}
+
+			continue;
+		}
+
+		if(_chunk[i] === '\\' && i < (_chunk.length - 1))
+		{
+			state[state.item] += _chunk[++i];
+			continue;
+		}
+
+		if(_chunk[i] === '\n')
+		{
+			if(_chunk[i + 1] === '\r')
+			{
+				++i;
+			}
+
+			++state.newLines;
+		}
+		else if(_chunk[i] === '\r')
+		{
+			if(_chunk[i + 1] === '\n')
+			{
+				++i;
+			}
+
+			++state.newLines;
+		}
+		else
+		{
+			if(state.newLines > 0)
+			{
+				if(_chunk.at(i, '### '))
+				{
+					pushItem();
+					state.item = '';
+					i += 3;
+					continue;
+				}
+
+				if(_chunk[i] === '#')
+				{
+					state.comment = true;
+					--state.newLines;
+					continue;
+				}
+			}
+
+			state.newLines = 0;
+
+			if(!state.item)
+			{
+				state.item = 'time';
+			}
+		}
+		
+		if(state.item) switch(state.item)
+		{
+			case 'time':
+				if(state.newLines > 0)
+				{
+					state.item = 'body';
+					continue loop;
+				}
+				
+				if(_chunk.at(i, ' # '))
+				{
+					state.item = 'head';
+					i += 2;
+					continue loop;
+				}
+				break;
+			case 'head':
+				if(state.newLines >= 2)
+				{
+					state.item = 'body';
+					continue loop;
+				}
+				break;
+			case 'body':
+				break;
+		}
+
+		if(state.item)
+		{
+			state[state.item] += _chunk[i];
+		}
+	}
 };
 
-const state = {};
+const pushItem = () => {
+	if(!(state.body = state.body.trim()))
+	{
+		return false;
+	}
+
+	items.push({
+		time: (state.time.trim() || '-/-'),
+		head: (state.head.trim() || '-/-'),
+		body: state.body });
+	
+	state.time = state.head = state.body = '';
+	return true;
+};
+
+const state = {
+	comment: false,
+	newLines: 0,
+	item: '',
+	time: '',
+	head: '',
+	body: '',
+};
 
 //
 

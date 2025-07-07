62c1c004bf8e1555ae81b5eae4e4e84cd37e1949
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2025-07-07 04:38:23 +0200

    [1751855901] ü

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,7 +3,7 @@
 /*
  * Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
  * https://kekse.biz/ https://github.com/kekse1/v4/
- * v0.0.1
+ * v0.1.0
  *
  * Helper script for my v4 project @ https://github.com/kekse1/v4/.
  *
@@ -14,14 +14,52 @@
  * TODO!
  */
 
+//
+const DEFAULT_BUFFER = (1024 * 256);
+const DEFAULT_ENCODING = 'utf8';
+
+//
 import { ready } from '../js/lib.js';
-var args = null; ready(() => {
-	args = getopt();
-	console.dir({args});
-	main(args);
-});
+
+//
+// --blog (.txt)
+// --json
+// --body (dir 4 .txt)
+//
+var args = null; ready(() => main(args = getopt()));
 
 const main = (_args = args) => {
-	console.error('TODO');
+	//
+	//todo/theor. sollte ich die argv[] genau pruefen;
+	//	... gehe aber erstmal von korrektheit aus.
+	//
+	
+	const stream = fs.createReadStream(
+		_args.get('blog'), {
+			encoding: DEFAULT_ENCODING,
+			autoClose: true,
+			emitClose: true,
+			highWaterMark: DEFAULT_BUFFER });
+	stream.once('end', (... _a) => finish(
+		stream, ... _a));
+	stream.on('data', (... _a) => chunk(
+		stream, ... _a));
 };
+
+const finish = (_stream) => {
+	console.dir('fin');
+};
+
+const chunk = (_stream, _chunk) => {
+	console.dir(_chunk.length);
+	parse(_chunk);
+};
+
+const parse = (_chunk) => {
+	//
+};
+
+const state = {};
+
+//
 

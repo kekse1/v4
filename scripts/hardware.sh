1cc7ef5a39c1fda119efa0c570fc7add2dd45002
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2026-08-14 13:17:04 +0200

    1786706222667688275

--- scripts/hardware.sh
+++ scripts/hardware.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#
+_TABS=1
+
+#
 if [[ ! -d ~/git/hardware/ ]]; then
 	echo " >> No '~/git/hardware/' directory found." >&2
 	exit 1
@@ -10,9 +14,17 @@ fi
 dirname="$(realpath ./)"
 cd ~/git/
 
+# -N ??
 tree -d --charset=UTF-8 --nolinks --noreport -- hardware/ >"${dirname}/main.txt"
 tree -d --charset=UTF-8 --nolinks --noreport -X -- hardware/ >"${dirname}/main.xml"
 tree -d --charset=UTF-8 --nolinks --noreport -J -- hardware/ >"${dirname}/main.json"
+
+if [[ $_TABS -gt 0 ]]; then
+	sed -i '1d' "${dirname}/main.txt"
+	tabs=""; for i in `seq 1 $_TABS`; do tabs+=$'\t'; done
+	sed -i "s/^/$tabs/" "${dirname}/main.txt"
+	echo " >> Inserted $_TABS tabs as line prefices in the 'main.txt'."
+fi
 
 echo " >> Just generated 'main.{txt,xml,json}'. :)~"
 echo " >> Location is '$dirname' (./)."
